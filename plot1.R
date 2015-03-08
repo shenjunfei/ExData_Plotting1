e3fc9df57d96285a58b82fa2a7282b9c2012d07d
Junfei Shen <shj735@gmail.com>
2015-03-08 15:48:24 -0400

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,12 @@
+proj<-read.table("household_power_consumption.txt",skip= 66637 ,nrows= 2881, sep=";")
+dt<-paste(proj$V1, proj$V2)
+date_time<-strptime(dt,"%d/%m/%Y %H:%M:%S")
+data<-cbind(date_time,proj[ ,3:9])
+names(data)<-c("Time", "Global_active_power", "Global_reactive_power", "Voltage",
+               "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+
+hist(data$Global_active_power, xlab="Global Active Power (kilowatts)", 
+     col="red", main="Global Active Power")
+
+dev.copy(png, file="plot1.png")
+dev.off()
